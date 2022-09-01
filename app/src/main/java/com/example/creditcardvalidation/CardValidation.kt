70ef77928901aa56ad03293fceeebef62b4f8269
Aren1219 <arenwang1219@gmail>
2022-09-01 09:56:28 +0100

    expiry date check improved

--- app/src/main/java/com/example/creditcardvalidation/CardValidation.kt
+++ app/src/main/java/com/example/creditcardvalidation/CardValidation.kt
@@ -1,18 +1,20 @@
+import java.text.SimpleDateFormat
+import java.util.*
+
 object CardValidation {
 
-    private val ACME = "1121"
-    private val ALFA = "1111"
-    private val AMEX = "3796"
+    private const val ACME = "1121"
+    private const val ALFA = "1111"
+    private const val AMEX = "3796"
 
     private fun checkFormat(cardNumber: String) =
         cardNumber.matches(Regex("[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}"))
 
     private fun checkDate(cardNumber: String, expiryDate: String): Boolean {
-//        val lastIndex = cardNumber.length - 1
-//        val month = cardNumber.substring(lastIndex - 3, lastIndex - 2).toInt()
-        return cardNumber.takeLast(4) == expiryDate.replace("/", "")
-//            month <= 12 && month >= 1 &&
-//            cardNumber.takeLast(2).toInt() >= 22
+        val expDate = SimpleDateFormat("MM/yy").parse(expiryDate)
+        val expMonth = expiryDate.take(2).toInt()
+        return cardNumber.takeLast(4) == expiryDate.replace("/", "") &&
+                expDate.compareTo(Date()) >= 0 && expMonth >= 1 && expMonth <= 12
     }
 
     private fun checkNumber(cardNumber: String): String?{
--- app/src/test/java/com/example/creditcardvalidation/ExampleUnitTest.kt
+++ app/src/test/java/com/example/creditcardvalidation/ExampleUnitTest.kt
@@ -13,32 +13,32 @@ class ExampleUnitTest {
 
     @Test
     fun `valid card number`(){
-        assertEquals("ACME", CardValidation.validation("1121-4356-1313-1222","12/22"))
-        assertEquals("ALFA", CardValidation.validation("1111-4356-1313-1222","12/22"))
-        assertEquals("AMEX", CardValidation.validation("3796-4356-1313-1222","12/22"))
+        assertEquals("ACME", CardValidation.validation("1121-4356-1313-1227","12/27"))
+        assertEquals("ALFA", CardValidation.validation("1111-4356-1313-1227","12/27"))
+        assertEquals("AMEX", CardValidation.validation("3796-4356-1313-1227","12/27"))
     }
 
     @Test
     fun `invalid format`() {
-        assertNull(CardValidation.validation("3796435613131222","12/22"))
-        assertNull(CardValidation.validation("1121-a356-1313-1222","12/22"))
-        assertNull(CardValidation.validation("1121.4356.1313.1222","12/22"))
-        assertNull(CardValidation.validation("1121-4356-14313-1222","12/22"))
-        assertNull(CardValidation.validation("1121-4356-1313-1222 ","12/22"))
-        assertNull(CardValidation.validation("1121-4356-1222","12/22"))
+        assertNull(CardValidation.validation("3796435613131227","12/27"))
+        assertNull(CardValidation.validation("1121-a356-1313-1227","12/27"))
+        assertNull(CardValidation.validation("1121.4356.1313.1227","12/27"))
+        assertNull(CardValidation.validation("1121-4356-14313-1227","12/27"))
+        assertNull(CardValidation.validation("1121-4356-1313-1222 ","12/27"))
+        assertNull(CardValidation.validation("1121-4356-1227","12/27"))
     }
 
     @Test
     fun `invalid date`() {
-        assertNull(CardValidation.validation("1121-4356-1313-1222","12/20"))
-        assertNull(CardValidation.validation("1121-4356-1313-1222","11/22"))
-//        assertNull(CardValidation.validation("1121-4356-1313-1219","12/19"))
-//        assertNull(CardValidation.validation("1121-4356-1313-1322","13/22"))
+        assertNull(CardValidation.validation("1121-4356-1313-1228","12/27"))
+        assertNull(CardValidation.validation("1121-4356-1313-1227","11/27"))
+        assertNull(CardValidation.validation("1121-4356-1313-1219","12/19"))
+        assertNull(CardValidation.validation("1121-4356-1313-1327","13/27"))
     }
 
     @Test
     fun `invalid card type`() {
-        assertNull(CardValidation.validation("1101-4356-1313-1222","12/22"))
-        assertNull(CardValidation.validation("0111-4356-1313-1222","12/22"))
+        assertNull(CardValidation.validation("1101-4356-1313-1227","12/27"))
+        assertNull(CardValidation.validation("0111-4356-1313-1227","12/27"))
     }
 }
