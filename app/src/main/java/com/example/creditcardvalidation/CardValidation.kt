d339b7a9e23b4ac7153c0552eee87f6926dd7bd7
Aren1219 <arenwang1219@gmail>
2022-09-06 16:50:48 +0100

    some code improvement

--- app/src/main/java/com/example/creditcardvalidation/CardValidation.kt
+++ app/src/main/java/com/example/creditcardvalidation/CardValidation.kt
@@ -1,34 +1,38 @@
+package com.example.creditcardvalidation
+
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.collections.HashMap
 
 object CardValidation {
 
-    private const val ACME = "1121"
-    private const val ALFA = "1111"
-    private const val AMEX = "3796"
+    private val cardTypes = HashMap<String, String>()
+    init {
+        cardTypes["ACME"] = "1121"
+        cardTypes["ALFA"] = "1111"
+        cardTypes["AMEX"] = "3796"
+    }
 
     private fun checkFormat(cardNumber: String) =
         cardNumber.matches(Regex("[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}"))
 
     private fun checkDate(cardNumber: String, expiryDate: String): Boolean {
-        val expDate = SimpleDateFormat("MM/yy").parse(expiryDate)
         val expMonth = expiryDate.take(2).toInt()
+        val expDate = SimpleDateFormat("MM/yy", Locale.UK).parse(expiryDate) ?: return false
         return cardNumber.takeLast(4) == expiryDate.replace("/", "") &&
-                expDate.compareTo(Date()) >= 0 && expMonth >= 1 && expMonth <= 12
+                expDate >= Date() && expMonth >= 1 && expMonth <= 12
     }
 
-    private fun checkNumber(cardNumber: String): String?{
-        return when(cardNumber.take(4)){
-            ACME -> "ACME"
-            ALFA -> "ALFA"
-            AMEX -> "AMEX"
-            else -> null
+    private fun checkType(cardNumber: String): String?{
+        for (type in cardTypes) {
+            if (type.value == cardNumber.take(4)) return type.key
         }
+        return null
     }
 
     fun validation(cardNumber: String, expiryDate: String): String? {
-        if (!checkFormat(cardNumber)||!checkDate(cardNumber, expiryDate)) return null
-        return checkNumber(cardNumber)
+        if (!checkFormat(cardNumber) || !checkDate(cardNumber, expiryDate)) return null
+        return checkType(cardNumber)
     }
 }
 
